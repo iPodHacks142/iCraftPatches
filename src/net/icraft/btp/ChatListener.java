4f17663c3ee24460d63dc431f913ae079272cdb6
Josh <ipodhacks142@hotmail.co.uk>
2015-07-16 14:16:00 +0100

    Players can now not start messages with '!' to prevent people bypassing mutes

---
+++ src/net/icraft/btp/ChatListener.java
@@ -0,0 +1,29 @@
+package net.icraft.btp;
+
+import java.io.IOException;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerChatEvent;
+
+
+@SuppressWarnings("deprecation")
+public class ChatListener 
+  implements Listener {
+
+	@SuppressWarnings({ "null" })
+	@EventHandler(priority=EventPriority.NORMAL)
+	  public void onPlayerChat(AsyncPlayerChatEvent chat){
+		Player player = (Player)chat.getPlayer();
+		String message = chat.getMessage();
+		if (message.startsWith("!")){
+			chat.setCancelled(true);
+			player.sendMessage(ChatColor.DARK_RED + "You cannot start messages with '!'"); return;
+		}
+	}
+
+}
